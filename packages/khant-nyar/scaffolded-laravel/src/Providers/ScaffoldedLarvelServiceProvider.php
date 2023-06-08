81178b0ef81d32e248f8646b90cbaa1dcfc041f7
Khant Si Thu Phyo <61083917+Khant-Nyar@users.noreply.github.com>
2023-06-09 02:48:43 +0630

    packages init

---
+++ packages/khant-nyar/scaffolded-laravel/src/Providers/ScaffoldedLarvelServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace khantnyar\scaffoldedLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScaffoldedLarvelServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/courier.php' => config_path('scaffolded-laravel.php'),
+            __DIR__ . '/../lang' => $this->app->langPath('vendor/courier'),
+        ]);
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'courier');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'courier');
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
