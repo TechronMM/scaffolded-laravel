77921a3d167181cce02a9da6530d4ffc781b440b
Khant Si Thu Phyo <61083917+Khant-Nyar@users.noreply.github.com>
2023-06-09 03:23:24 +0630

    generate necessary file for packages

--- packages/khant-nyar/scaffolded-laravel/src/Providers/ScaffoldedLarvelServiceProvider.php
+++ packages/khant-nyar/scaffolded-laravel/src/Providers/ScaffoldedLarvelServiceProvider.php
@@ -1,24 +1,27 @@
 <?php
 
-namespace khantnyar\scaffoldedLaravel;
+namespace KhantNyar\ScaffoldedLaravel\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
 class ScaffoldedLarvelServiceProvider extends ServiceProvider
 {
+    protected $vendorName = 'khant-nyar';
+    protected $packageName = 'Scaffolded-laravel';
     /**
      * Register services.
      */
     public function register(): void
     {
-        $this->publishes([
-            __DIR__ . '/../config/courier.php' => config_path('scaffolded-laravel.php'),
-            __DIR__ . '/../lang' => $this->app->langPath('vendor/courier'),
-        ]);
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'courier');
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'courier');
+        // $this->publishes([
+        //     __DIR__ . '/../config/courier.php' => config_path('scaffolded-laravel.php'),
+        //     __DIR__ . '/../lang' => $this->app->langPath('vendor/courier'),
+        // ]);
+        // $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        // $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+        // $this->loadViewsFrom(__DIR__ . '/../resources/views', 'courier');
+        // $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        // $this->loadTranslationsFrom(__DIR__ . '/../lang', 'courier');
     }
 
     /**
@@ -26,6 +29,6 @@ class ScaffoldedLarvelServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        include __DIR__ . '/../routes/api.php';
     }
 }
